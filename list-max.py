e463bbe367532fba9bb3db717c1f8438966f2e62
jiahongwang <85048990@qq.com>
2019-06-07 17:59:23 +0800

    list-max.py

---
+++ list-max.py
@@ -0,0 +1,18 @@
+
+# Author:JiaHongWang
+# -*- coding utf-8 -*-
+
+nums=[19,20,33,55,56,77]
+index=0
+index_id=0
+max_num=0
+for num in nums:
+    if num>max_num:
+        max_num=num
+        index=index_id
+    index_id+=1
+print('max_num:%d,nums[%d]'% (max_num,index))
+
+
+
+
